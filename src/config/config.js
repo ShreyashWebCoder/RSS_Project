60454f95bb6644402f95ed8254eb45e42c6eac33
ShreyashWebCoder <shreyashwebcoder@gmail.com>
2025-08-28 17:17:24 +0530

    search update

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || 'your_jwt_secret',
-    dbUri: process.env.DB_URI || 'mongodb://localhost:27017/RSS_DB',
+    dbUri: process.env.DB_URI,
 };
 
 export default config;
